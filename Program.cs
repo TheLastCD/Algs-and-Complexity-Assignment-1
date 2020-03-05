a82b5df91a42dd33f15b0309eee390aad97a6278
Charlie N A <lambopuggy@gmail.com>
2020-03-05 12:14:59 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,311 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algs_and_Complexity_Assignment_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string sorting, filesort = "", orderby;
+            int operations = 0,TenFift = 10;
+            List<int> File = new List<int>(),
+                tenths = new List<int>();
+            var l = new Sorting_Algorithms();
+            l.Populate_Lists();
+
+            Console.WriteLine("What file is to be sorted");
+            Console.WriteLine("1) Net_1_256 \n2) Net_2_256 \n3) Net_3_256 \n4) Net_1_2048 \n5)Net_2_2048 \n6)Net_3_2048");
+            int file = Verify_value();
+            switch (file)
+            {
+                case 1:;
+                    filesort = "Net_1_256";
+                    File = l.Net_1_256;
+                    break;
+                case 2:;
+                    filesort = "Net_2_256";
+                    File = l.Net_2_256;
+                    break;
+                case 3:;
+                    filesort = "Net_3_256";
+                    File = l.Net_3_256;
+                    break;
+                case 4:;
+                    filesort = "Net_1_2048";
+                    File = l.Net_1_2048;
+                    TenFift = 50;
+                    break;
+                case 5:;
+                    filesort = "Net_2_2048";
+                    File = l.Net_2_2048;
+                    TenFift = 50;
+                    break;
+                case 6:
+                    filesort = "Net_3_2048";
+                    File = l.Net_3_2048;
+                    TenFift = 50;
+                    break;
+                default:
+                    Console.WriteLine("Error Net_1_256 Selected by default:");
+                    filesort = "Test";
+                    File = l.Test;
+                    break;
+            }
+            Console.Clear();
+
+
+            Console.WriteLine("Do you want it sorted in:");
+            Console.WriteLine("1) Ascending \n2) Descending");
+            int asci = Verify_value();
+            bool asc = true;
+            switch (asci)
+            {
+                case 1:
+                    orderby = "Ascending";
+                    break;
+                case 2:
+                    orderby = "Descending";
+                    asc = false;
+                    break;
+                default:
+                    orderby = "Ascending";
+                    break;
+
+            }
+            Console.Clear();
+            
+            Console.WriteLine("which Sorting algorithm would you like to use?");
+            Console.WriteLine("1) Merge Sort\n2) Quick Sort\n3) Insertion Sort\n4) Bubble Sort");
+            int sortingChoice = Verify_value();
+            Console.Clear();
+            switch (sortingChoice)
+            {
+                case 1:
+                    File = l.Merge_Sort(File, asc);
+                    operations = l.operations;
+                    sorting = "Merge";
+                    break;
+                case 2:
+                    File = l.Quick_Sort(File, asc);
+                    operations = l.operations;
+                    sorting = "Quick";
+                    break;
+                case 3:
+                    File = l.Insertion_Sort(File, asc);
+                    operations = l.operations;
+                    sorting = "Insertion";
+                    break;
+                case 4:
+                    File = l.Bubble_Sort(File, asc);
+                    operations = l.operations;
+                    sorting = "Bubble";
+                    break;
+                default:
+                    File = l.Merge_Sort(File, asc);
+                    operations = l.operations;
+                    sorting = "Merge";
+                    break;
+            }
+
+
+            Console.WriteLine($" you selected the {sorting} Sort\n performed on {filesort}\n and ordered in {orderby}");
+            Console.WriteLine($"Number of Operations = {operations}");
+            Console.WriteLine($"now that the Array has been sorted would you like to: \n1) display every {TenFift}th value\n2) Search the Array ");
+            Console.Write("\n");
+            int searchorShow = Verify_value();
+            switch (searchorShow)
+            {
+                case 1:
+                    foreach (int k in l.Get_Tenth(File,TenFift))
+                    {
+                        Console.Write($"{k}, ");
+                    }
+                    break;
+                case 2:
+                    Searching(File);
+                    break;
+                default:
+                    break;
+
+            }
+            Console.Write("\n");
+            
+
+        }
+
+        public static int Verify_value()
+        {
+            bool exit = false;
+            while (!exit)
+            {
+                try
+                {
+                    int ToConvert = Convert.ToInt32(Console.ReadLine());
+                    exit = true;
+                    return ToConvert;
+                }
+                catch
+                {
+                    Console.WriteLine("There has been an error try again");
+                }
+            }
+            return 1;
+        }
+        public static void Searching(List<int> SortedArray)
+        {
+            var x = new Searching_Algorithms();
+            string search = "Linear";
+            List <int> Indexes = new List<int>();
+            Console.Clear();
+            Console.WriteLine("What Search would you like to perform: \n1) Linear Search \n2) Binary Search");
+            int SearchingChoice = Verify_value();
+            switch (SearchingChoice)
+            {
+                case 1:
+                    break;
+                case 2:
+                    search = "Binary";
+                    break;
+                default:
+                    break;  
+            }
+            Console.WriteLine("What value would you like to search for?");
+            int ValueSearch = Verify_value();
+            switch (search)
+            {
+                case "Linear":
+                    Indexes = x.Linear_Search(SortedArray, ValueSearch);
+                    break;
+                case "Binary":
+                    //Index = x.Binary_Search(SortedArray, ValueSearch);
+                    break;
+                default:
+                    Indexes = x.Linear_Search(SortedArray, ValueSearch);
+                    break;
+            }
+            if(SortedArray[Indexes[0]] != ValueSearch)
+            {
+                Console.WriteLine($"The number cannot be found \n the closest value to it however is at Index: {Indexes[0]}, Value: {SortedArray[Indexes[0]]}\n which is {ValueSearch -SortedArray[Indexes[0]]} away from the target {ValueSearch}");
+            }
+            else
+            {
+                Console.WriteLine($"The Value has been found at index(es): ");
+                foreach (int k in Indexes)
+                {
+                    Console.Write($"{k}, ");
+                }
+            }
+
+        }
+    }
+   
+
+
+    public class Searching_Algorithms
+    {
+        public int Binary_Search(List<int> array, int ValueFind)
+        {
+            bool found = false, thing = false;
+            List<int> fudge = new List<int>();
+            int index = -1,
+                first = 0,
+                last = array.Count - 1,
+                midpoint,
+                nearLow = 0,
+                nearHigh = array.Count,
+                highTemp = array.Count +1;
+            while (first <= last && !found)
+            {
+                midpoint = Convert.ToInt32(Math.Floor((double)((first + last) / 2)));
+                if (array[midpoint] == ValueFind)
+                {
+                    index = midpoint;
+                    array[index] = -1;
+                    foreach (int lm in array)
+                        Console.Write($"{lm}, ");
+                    fudge.Add(index);
+                    while (!thing)
+                    {
+                        int xlm = Binary_Search(array, ValueFind);
+                        if (xlm == -1)
+                            thing = true;
+                        else
+                        {
+                            if (array[xlm] == ValueFind)
+                                fudge.Add(index);
+                            else thing = true;
+
+                        }
+
+                    }
+                    
+                    
+                }
+                else
+                {
+                    if (array[midpoint] < ValueFind)
+                    {
+                        first = midpoint + 1;
+                        nearLow = midpoint;
+                    }
+                    else
+                    {
+                        last = midpoint - 1;
+                    }
+                }
+            }
+            nearHigh = nearLow + 1;
+            //if (index == -1)
+            //{
+            //    while (array[nearHigh] == array[nearLow])
+            //        nearHigh++;
+            //    if ((array[nearHigh] - ValueFind) > (ValueFind - array[nearLow]))
+            //        index = array[nearLow];
+            //    else index = array[nearHigh];
+            //}
+            foreach (int turd in fudge)
+                Console.Write($"{turd}, ");
+
+            return index;
+        }
+
+        public List<int> Linear_Search(List<int> array, int ValueFind)
+        {
+            List<int> MultipleIndex = new List<int>();
+            int TempHoldL = 0, TempHoldH = array.Count - 1;
+            bool Found = false;
+            for(int i = 0; i < array.Count(); i++)
+            {
+                if(array[i] == ValueFind)
+                {
+                    MultipleIndex.Add(i);
+                    Found = true;
+                }
+                else
+                {
+                    if(array[i] < ValueFind)
+                        TempHoldL = i;
+                    else
+                    {
+                        TempHoldH = i;
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine($"Low: {TempHoldL}, High: {TempHoldH}");
+            if (!Found)
+            {
+                if ((array[TempHoldH] - ValueFind) < (ValueFind - array[TempHoldL]))
+                    MultipleIndex.Add(TempHoldH);
+                else
+                    MultipleIndex.Add(TempHoldL);
+            }            
+            return MultipleIndex;
+        }
+    }
+}
+
---
+++ Sorting_Algorithms.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algs_and_Complexity_Assignment_1
+{
+    public class Sorting_Algorithms
+    {
+        private int _ReferenceIndex = 10, _operations = 0, _interval = 10;
+        public int ReferenceIndex
+        {
+            get { return _ReferenceIndex; }
+            set
+            {
+                switch (value)
+                {
+                    case 0:
+                        _ReferenceIndex += interval;
+                        break;
+                    case 10:
+                        _ReferenceIndex = 10;
+                        break;
+                    case 50:
+                        _ReferenceIndex = 50;
+                        break;
+                }
+                
+            } 
+        }
+        public int interval
+        {
+            get { return _interval; }
+            set
+            {
+                _interval = value;
+            }
+        }
+
+        public int operations
+        {
+            get { return _operations; }
+            set
+            {
+                if (value == 0)
+                {
+                    _operations += 1;
+                }
+                if (value == 1)
+                {
+                    _operations = 0;
+                };
+
+            }
+        }
+
+
+        public List<int> Net_1_256 = new List<int>(),
+            Net_2_256 = new List<int>(),
+            Net_3_256 = new List<int>(),
+            Net_1_2048 = new List<int>(),
+            Net_2_2048 = new List<int>(),
+            Net_3_2048 = new List<int>(),
+            Test = new List<int>();
+
+        public void Populate_Lists()
+        {
+            Net_1_256 = Array.ConvertAll(File.ReadAllLines(@"Net_1_256.txt"), s => int.Parse(s)).ToList();
+            Net_2_256 = Array.ConvertAll(File.ReadAllLines(@"Net_2_256.txt"), s => int.Parse(s)).ToList();
+            Net_3_256 = Array.ConvertAll(File.ReadAllLines(@"Net_3_256.txt"), s => int.Parse(s)).ToList();
+            Net_1_2048 = Array.ConvertAll(File.ReadAllLines(@"Net_1_2048.txt"), s => int.Parse(s)).ToList();
+            Net_2_2048 = Array.ConvertAll(File.ReadAllLines(@"Net_2_2048.txt"), s => int.Parse(s)).ToList();
+            Net_3_2048 = Array.ConvertAll(File.ReadAllLines(@"Net_3_2048.txt"), s => int.Parse(s)).ToList();
+            Test = Array.ConvertAll(File.ReadAllLines(@"Test.txt"), s => int.Parse(s)).ToList();
+            Console.WriteLine("");
+        }
+
+        public List<int> Get_Tenth(List<int> array, int selectedInterval)
+        {
+
+            ReferenceIndex = selectedInterval;
+            List<int> tenths = new List<int>();
+            while(ReferenceIndex <= array.Count)
+            {
+                tenths.Add(array[ReferenceIndex-1]);
+                ReferenceIndex = 0;
+            }
+            ReferenceIndex = interval;
+            return tenths;
+
+        }
+
+        public int Find_Value()
+        {
+            return 1;
+        }
+
+       
+
+
+        //sorts
+        public List<int> Merge_Sort(List<int> array, bool asc)
+        {
+            int mid = array.Count / 2;
+            List<int> lefthalf = new List<int>(),
+                righthalf = new List<int>();
+            if (array.Count > 1)
+            {
+                for (int count = 0; count < array.Count; count++)
+                {
+                    if (count < mid)
+                    {
+                        lefthalf.Add(array[count]);
+                    }
+                    if (count >= mid)
+                    {
+                        righthalf.Add(array[count]);
+                    }
+                }
+
+
+                Merge_Sort(lefthalf, asc);
+                Merge_Sort(righthalf, asc);
+                int i = 0, j = 0, k = 0;
+
+                while (i < lefthalf.Count && j < righthalf.Count)
+                {
+
+                    if (lefthalf[i] < righthalf[j] && asc)
+                    {
+                        array[k] = lefthalf[i];
+                        i++;
+                    }
+                    else
+                    {
+                        if (lefthalf[i] > righthalf[j] && !asc)
+                        {
+                            array[k] = lefthalf[i];
+                            i++;
+                        }
+                        else
+                        {
+                            array[k] = righthalf[j];
+                            j++;
+                        }
+                    }
+                    k++;
+                    operations = 0;
+                }
+                while (i < lefthalf.Count)
+                {
+                    array[k] = lefthalf[i];
+                    i++;
+                    k++;
+                }
+                while (j < righthalf.Count)
+                {
+                    array[k] = righthalf[j];
+                    j++;
+                    k++;
+                }
+            }
+            return array;
+        }
+        public List<int> Bubble_Sort(List<int> array, bool asc)
+        {
+            int number = array.Count();
+            for (int i = 0; i < number - 1; i++)
+            {
+                for (int j = 0; j < number - 1; j++)
+                {
+                    if (array[j + 1] < array[j] && asc)
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                    if (array[j + 1] > array[j] && !asc)
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                    operations = 0;
+                }
+            }
+
+
+
+            return array;
+        }
+        public List<int> Insertion_Sort(List<int> array, bool asc)
+        {
+            int numSorted = 1, index;
+            while (numSorted < array.Count)
+            {
+                int temp = array[numSorted];
+                for (index = numSorted; index > 0; index--)
+                {
+                    if (temp < array[index - 1] && asc)
+                        array[index] = array[index - 1];
+                    else
+                    {
+                        if (temp > array[index - 1] && !asc)
+                            array[index] = array[index - 1];
+                        else break;
+                    }
+
+
+                }
+                array[index] = temp;
+                numSorted++;
+                operations = 0;
+            }
+            return array;
+        }
+
+        public List<int> Quick_Sort(List<int> array, bool asc)
+        {
+            return Recursion(array, 0, array.Count - 1, asc);
+        }
+        private List<int> Recursion(List<int> array, int left, int right, bool asc)
+        {
+            int i = left,
+                j = right,
+                pivot = array[(left + right) / 2],
+                temp;
+            do
+            {
+                if (asc)
+                {
+                    while (array[i] < pivot && i < right) i++;
+                    while (pivot < array[j] && j > left) j--;
+                }
+                else
+                {
+                    while (array[i] > pivot && i < right) i++;
+                    while (pivot > array[j] && j > left) j--;
+                }
+                if (i <= j)
+                {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+                operations = 0;
+            } while (i <= j);
+            if (left < j)
+                Recursion(array, left, j, asc);
+            if (i < right)
+                Recursion(array, i, right, asc);
+            return array;
+
+        }
+    }
+}
